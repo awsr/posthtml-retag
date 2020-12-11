0594e035cfa7925cb31a5364b5ef884daa2ae31b
awsr <43862868+awsr@users.noreply.github.com>
2020-12-11 06:09:50 +0000

    Prettify lib files

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,6 +1,6 @@
 declare const _default: (options?: RTOptions) => Function;
 export default _default;
 interface RTOptions {
-    attr?: string;
-    removeDisplayNone?: boolean;
+  attr?: string;
+  removeDisplayNone?: boolean;
 }

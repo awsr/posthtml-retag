b20b3ff43f64ec0d7210af51e66e231aee894680
awsr <43862868+awsr@users.noreply.github.com>
2020-12-12 22:27:54 +0000

    Improved usefulness of type hinting preview

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,6 +1,1 @@
-declare const _default: (options?: RTOptions) => Function;
-export default _default;
-interface RTOptions {
-  attr?: string;
-  removeDisplayNone?: boolean;
-}
+export default function posthtmlReTag(options?: { attr?: string; removeDisplayNone?: boolean }): Function;
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
 import { Node } from "posthtml";
 
-export default (options: RTOptions = {}): Function => {
+export default function posthtmlReTag(options: {attr?: string; removeDisplayNone?: boolean} = {}): Function {
   options.attr = options.attr || "retag";
   options.removeDisplayNone = options.removeDisplayNone || false;
 
-  function removeDisplayNone(rule: string) {
+  function removeDN(rule: string) {
     return !/display:\s*none/i.test(rule) && rule.length > 0;
   }
 
-  return function posthtmlReTag(tree: Node): Node {
+  return function process(tree: Node): Node {
     tree.walk(node => {
       if (node.attrs && node.attrs.hasOwnProperty(options.attr as string)) {
         const newTag = node.attrs[options.attr as string];
@@ -17,7 +17,7 @@ export default (options: RTOptions = {}): Function => {
           node.tag = newTag.trim();
 
           if (options.removeDisplayNone && typeof node.attrs.style === "string") {
-            node.attrs.style = node.attrs.style.split(";").filter(removeDisplayNone).join(";").trim();
+            node.attrs.style = node.attrs.style.split(";").filter(removeDN).join(";").trim();
 
             if (node.attrs.style.length === 0) {
               delete node.attrs.style;
@@ -36,10 +36,5 @@ export default (options: RTOptions = {}): Function => {
     return tree;
   };
 };
-
-interface RTOptions {
-  attr?: string;
-  removeDisplayNone?: boolean;
-}
 
 module.exports = exports.default;

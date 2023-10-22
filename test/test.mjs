b96b654e1316c271f67c5ad1410d04d349052044
awsr <43862868+awsr@users.noreply.github.com>
2023-10-21 21:53:02 -0700

    Add test resources

---
+++ test/test.mjs
@@ -0,0 +1,81 @@
+import assert from "assert";
+import test from "node:test";
+
+import posthtml from "posthtml";
+import retag from "posthtml-retag";
+
+
+const testPairs = [
+  [
+    // 0
+    '<span retag="h1">Test for <code>&lt;span&gt;</code> to <code>&lt;h1&gt;</code></span>',
+    "<h1>Test for <code>&lt;span&gt;</code> to <code>&lt;h1&gt;</code></h1>",
+  ],
+  [
+    // 1
+    '<p id="test-id" retag="div" class="test-class">Test for not modifying unrelated attributes</p>',
+    '<div id="test-id" class="test-class">Test for not modifying unrelated attributes</div>',
+  ],
+  [
+    // 2
+    '<span changeto="h2">Test for custom attribute name</span>',
+    "<h2>Test for custom attribute name</h2>",
+  ],
+  [
+    // 3
+    '<div retag="noscript" style="display: none;">Test <code>removeDisplayNone</code></div>',
+    "<noscript>Test <code>removeDisplayNone</code></noscript>",
+  ],
+  [
+    // 4
+    '<span retag="p">Test for <span retag="code">nested retagging</span></span>',
+    "<p>Test for <code>nested retagging</code></p>",
+  ],
+  [
+    // 5
+    '<div retag="h2" style="color: darkblue; display: none; border: 1px solid #777; background-color: #fff;">Test for removing <code>display: none</code> and leaving style attribute</div>',
+    '<h2 style="color: darkblue; border: 1px solid #777; background-color: #fff">Test for removing <code>display: none</code> and leaving style attribute</h2>',
+  ],
+  [
+    // 6
+    '<span retag="small" style="display: none !important;">Test for overriding <code>removeDisplayNone</code> removal</span>',
+    '<small style="display: none !important">Test for overriding <code>removeDisplayNone</code> removal</small>',
+  ],
+];
+
+console.log("\nTesting ES Module format");
+
+test("Test span to h1", async () => {
+  await runTest(...testPairs[0]);
+});
+
+test("Test for not modifying unrelated attributes", async () => {
+  await runTest(...testPairs[1]);
+});
+
+test("Test for custom attribute name", async () => {
+  await runTest(...testPairs[2], { attr: "changeto" });
+});
+
+test("Test removeDisplayNone", async () => {
+  await runTest(...testPairs[3], { removeDisplayNone: true });
+});
+
+test("Test for nested retagging", async () => {
+  await runTest(...testPairs[4]);
+});
+
+test("Test for removeDisplayNone not affecting other style declarations", async () => {
+  await runTest(...testPairs[5], { removeDisplayNone: true });
+});
+
+test("Test for ignoring '!important'", async () => {
+  await runTest(...testPairs[6], { removeDisplayNone: true });
+});
+
+
+async function runTest(html, expected, options = {}) {
+  return posthtml([retag(options)])
+    .process(html)
+    .then((result) => assert.strictEqual(result.html, expected));
+}

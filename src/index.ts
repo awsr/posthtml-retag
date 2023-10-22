ee5f1cf1deaa9dbff10983cbde1bc6c518f9631a
awsr <43862868+awsr@users.noreply.github.com>
2023-10-22 02:40:22 -0700

    Minor documentation fix

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import type { Node } from "posthtml";
  * @param {Object} [options] - Options object for PostHTML-ReTag.
  * @param {string} [options.attr="retag"] - Name of the attribute that contains the new tag. Defaults to `retag`.
  * @param {boolean} [options.removeDisplayNone=false] - Also remove `display: none` from the style attribute
- * of the element being converted unless it is marked as `important`. If the style attribute is empty after
+ * of the element being converted unless it is marked as `!important`. If the style attribute is empty after
  * conversion, it is removed. Defaults to `false`.
  */
 export function retag(options: { attr?: string; removeDisplayNone?: boolean } = {}): (tree: Node) => Node {
